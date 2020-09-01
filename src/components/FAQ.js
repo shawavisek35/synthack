700d120ed848c1ad651dceaa04fd5e18a7c99a38
Hasibul Islam <star.sazzad@gmail.com>
2020-09-01 23:32:51 +0530

    Update FAQ.js
    Added Extra FAQ

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -45,6 +45,26 @@ const FAQs = [
       "You should contact the mentor or any of the organisers on Discord channel. The college authority is nowhere responsible in resolving your doubts or disputes.",
   },
   {
+    q: "Do I need to have extensive programming/coding experience to get started?",
+    a: "Absolutely NOT! There are projects in varying difficulty levels and if you are a beginner, you will still be welcomed.",
+  },
+  {
+    q: "Do I need to know how GitHub works or any other Version Control System will work?",
+    a: "Since the projects are hosted on GitHub, we expect that you have the knowledge of how GitHub works.",
+  },
+  { 
+    q: "What would happen if participant couldn't complete the task assigned by their mentors?",
+    a: "If not in the time period, they are not qualified for certificates",
+  },
+  {
+    q: "Will participant get participation certificate if they leave the competition in the middle or if they cannot complete the task?",
+    a: "No. To receive the certificates one must complete the tasks within the time period given by the mentor",
+  },
+  {
+    q: "What are the perks of participating in this event?"
+    a: "All the qualified participants will receive a certificate and top performers will receive special schwag",
+  },
+  {
     q: ` Do I need to pay any fees to take part in this event? `,
     a: `No. You need not to pay a single penny to participate in this event.`,
   },
