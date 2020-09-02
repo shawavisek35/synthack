b987c04813b5889433607d5d8e41488473f7dddd
Sagnik Chattopadhyaya <36898274+sagnik20@users.noreply.github.com>
2020-09-02 20:17:05 +0530

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,76 +1,43 @@
 import React from "react";
+import "./assets/team.css";
 const Team = () => {
   return (
     <div style={{ textAlign: "center" }}>
       <h1 className="balsamiq rounded lg:text-6xl md:text-6xl sm:text-5xl text-6xl text-center ">
         Team
       </h1>
-      <div style={{ marginTop: "2%" }}>
-    <a href="https://studentambassadors.microsoft.com/en-US/profile/4447/">
-        <img
-          src={require("./Images/sagnik.png")}
-          style={{
-            height: "200px",
-            width: "200px",
-            marginLeft: "20%",
-            float: "left",
-            paddingRight: "15px",
-            marginRight: "5%",
-          }}
-          alt="Sagnik-img"
-        ></img>
-        <br />
-
-        <h1 style={{ textAlign: "left", fontSize: "2vw" }}>
-          
-            <b>Sagnik Chattopadhyaya</b>
-          
-        </h1>
-
-        <p style={{ textAlign: "left" }}>
-          <em> Microsoft Learn Student Ambassador at CIEM, Kolkata.</em>
-          <br />
-          <br /> I'm enthusiastic about coding and trying to automate my life 1%
-          at a time.
-          <br /> I plan on integrating ML, Web and AI in the future for the
-          betterment of mankind.
-        </p>
-
-        </a>
-      </div>
-
-      <div style={{ marginTop: "8%", marginBottom: "10%" }}>
-        <a href="http://avisekcode.netlify.app/">
-        <img
-          src={require("./Images/avisek.png")}
-          style={{
-            height: "200px",
-            width: "200px",
-            marginLeft: "20%",
-            float: "left",
-            paddingRight: "15px",
-            marginRight: "5%",
-          }}
-          alt="avisek-img"
-        ></img>
-
-        <br />
-
-        <h1 style={{ textAlign: "left", fontSize: "2vw" }}>
-          
-            <b>Avisek Shaw</b>
-          
-        </h1>
-
-        <p style={{ textAlign: "left" }}>
-          <em>Developer Student Clubs Lead at CIEM, kolkata.</em>
-          <br />
-          <br /> Data Science enthusiast and a competitive programmer.
-          <br /> Love to automate things through scripting.
-        </p>
-        </a>
+      
+<a href="https://studentambassadors.microsoft.com/en-US/profile/4447/">
+  <div class="row">
+  <div class="column">
+    <div class="card">
+      <img src={require("./Images/sagnik.png")} alt="Jane" style={{width:100%}}>
+      <div class="container">
+        <h2>Sagnik Chattopadhyaya</h2>
+        <p class="title">Microsoft Learn Student Ambassador at CIEM</p>
+        <p>I'm enthusiastic about coding and trying to automate my life 1%
+          at a time. I plan on integrating ML, Web and AI in the future for the
+          betterment of mankind.</p>
+        <p><button class="button">Contact</button></p>
       </div>
     </div>
+  </div>
+</a>
+
+<a href="http://avisekcode.netlify.app/">
+  <div class="row">
+  <div class="column">
+    <div class="card">
+      <img src={require("./Images/avisek.png")} alt="Avisek" style={{width:100%}}>
+      <div class="container">
+        <h2>Avisek Shaw</h2>
+        <p class="title">Developer Student Clubs Lead at CIEM</p>
+        <p>Data Science enthusiast and a competitive programmer. Love to automate things through scripting.</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</a>
   );
 };
 
