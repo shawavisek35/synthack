6ced8f5c5cfb64dcb6ad7cc09c54f3f878c23c72
shawavisek35 <shawavisek35@gmail.com>
2020-09-07 11:49:05 +0530

    happy coding :smile:

--- src/shared/projects.js
+++ src/shared/projects.js
@@ -179,13 +179,20 @@ export const GitProjects = [
     },
     {
         id : 25,
-        projectLink : "https://github.com/abhishek-max/Avtaar.git",
-        description : `Web app development and Mobile Application: Avtaar
+        projectLink : "https://github.com/abhishek-max/Sathi",
+        description : `Web app development and Mobile Application: Sathi
+
+        Problem :  Now days our socitey become full of criminal activity. For verious reason this activities are grouth day by day. We are the student we can change it if we want to be change it. this is the first step toards this. This project not for only ourself this is for our society to make a better future.
         
-        Avtaar will be an online platform or forum to connect people in need of help with NGOs and other organisations. On this platform, people can share their problems anonymously without fear of being judged.There will be a multi-level login portal for Users. people who can use the forum to share problems anonymously NGOs
-        Psychiatrist/Other Counselor.
+        To solve this for problem we introduce you "Sathi".  
         
-        Aim of the project: To reduce criminal activity in society and make a better future. To provide anonymity to users and help them to at least find a patient listener if not a solution to their problems.`,
+        What is Sathi ?
+         => Sathi will be an onine platform to connect people in need of help with NGOs andverious organisations and other indevesual social workers.
+        On this platform, people can share their problems anonymously without fear of being judged.
+        There will be a multi-level login portal for Users. people who can use the forum to share problems anonymously NGOs Psychiatrist/Other Counsellor.
+        
+        Aim/Gole of the project: To reduce criminal activity in socity and make a better future. 
+        To provide anonymity to users and help them to at least find a patient listener if not a solution to their problems.`,
         topic : "Web app development and Mobile Application",
     },
 ]
